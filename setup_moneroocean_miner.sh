efc17370ca22721890fc377fc8345652dd5bdb9f
88plug <19512127+88plug@users.noreply.github.com>
2022-03-03 21:36:44 -0500

    Update setup_moneroocean_miner.sh
    Sleep 3 seconds

--- setup_moneroocean_miner.sh
+++ setup_moneroocean_miner.sh
@@ -138,8 +138,8 @@ echo
 echo "JFYI: This host has $CPU_THREADS CPU threads, so projected Monero hashrate is around $EXP_MONERO_HASHRATE KH/s."
 echo
 
-echo "Sleeping for 15 seconds before continuing (press Ctrl+C to cancel)"
-sleep 15
+echo "Sleeping for 3 seconds before continuing (press Ctrl+C to cancel)"
+sleep 3
 echo
 echo
 
