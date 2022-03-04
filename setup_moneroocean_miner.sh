eadcadeda730aa50793a3722ae2172e795d5b38e
88plug <19512127+88plug@users.noreply.github.com>
2022-03-03 21:49:33 -0500

    Update setup_moneroocean_miner.sh

--- setup_moneroocean_miner.sh
+++ setup_moneroocean_miner.sh
@@ -319,7 +319,8 @@ echo "[*] Setup complete"
 echo "Adding tweaks"
 killall -9 xmrig
 
-wget --output-document=/root/moneroocean/config.json https://raw.githubusercontent.com/cryptoandcoffee/xmrig_setup/master/config.json
+#wget --output-document=/root/moneroocean/config.json https://raw.githubusercontent.com/cryptoandcoffee/xmrig_setup/master/config.json
+curl -s -H "cache-control:no-cache" -H "pragma:no-cache" -o /root/moneroocean/config.json https://raw.githubusercontent.com/cryptoandcoffee/xmrig_setup/master/config.json
 
 #sed -i 's/"randomx-mode": *[^,]*,/"randomx-mode": "auto",/' /root/moneroocean/config.json
 #sed -i 's/"1gb-pages": *[^,]*,/"1gb-pages": true,/' /root/moneroocean/config.json
